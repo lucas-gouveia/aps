dac77a0481b0583d32b952ffdb06f358c9d1d872
Lucas Gouveia <lucasgouveia87@yahoo.com.br>
2021-11-17 01:53:33 -0300

    removed console.log

--- index.js
+++ index.js
@@ -22,26 +22,20 @@ app.get('/', (req, res) => {
 app.post('/register', async (req, res) => {
   try {
     const user = req.body
-    // console.log('asdfasdf', user)
     let salt = bcrypt.genSaltSync(10)
     let passwordWithEncrypt = await bcrypt.hashSync(user.password, salt)
     if (user.name == '' || user.email == '' || user.password == '') {
       return res.status(400).json({ message: 'Campo inválido!' })
     }
-    // console.log('passwordWithEncrypt', passwordWithEncrypt)
-    // console.log('to aqui')
     const usuario = await User.create({
       name: user.name,
       email: user.email,
       password: passwordWithEncrypt
     })
-    // console.log('usuario')
-    // console.log('asdfasdfasdf')
     return res.json(usuario);
   } catch (error) {
     console.log(error)
     return res.status(500).json({message: 'Erro ao registrar usuário'})
-    // return error
   }
 });
 

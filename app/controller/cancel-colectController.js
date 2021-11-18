5ca458333ce6e609aa24199f85a2a2c51dc67f39
Lucas Gouveia <lucasgouveia87@yahoo.com.br>
2021-11-18 20:35:03 -0300

    cancel controller

--- app/controller/cancel-colectController.js
+++ app/controller/cancel-colectController.js
@@ -9,14 +9,17 @@ export const cancelColect = async (req, res) => {
         id: identificador.id,
       }
     })
+    if (userId !== colects.dataValues.user_id) {
+      return res.status(401).json({ message: 'Não autorizado!' })
+    }
     if (colects == null) {
       return res.status(400).json({ message: 'Coleta não encontrada!' })
     }
     if (colects.dataValues.canceled == true) {
       return res.status(400).json({ message: 'Coleta já cancelada!' })
     }
-    if (userId !== colects.dataValues.user_id) {
-      return res.status(401).json({ message: 'Não autorizado!' })
+    if (colects.dataValues.conclused == true) {
+      return res.status(400).json({ message: 'Coleta não pode ser cancelada!' })
     }
     colects.canceled = true
     await colects.save()

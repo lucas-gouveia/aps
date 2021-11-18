397f153e07e05e3d6f1c68cce83d41fd3eaa9082
Lucas Gouveia <lucasgouveia87@yahoo.com.br>
2021-11-18 19:54:17 -0300

    include more columns on model

---
+++ app/models/colect.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) => {
+  const Colect = sequelize.define('Colects', {
+    user_id: DataTypes.INTEGER,
+    description: DataTypes.STRING,
+    material: DataTypes.STRING,
+    avenue: DataTypes.STRING,
+    number: DataTypes.STRING,
+    district: DataTypes.STRING,
+    city: DataTypes.STRING,
+    state: DataTypes.STRING(2),
+    postal_code: DataTypes.STRING(8),
+    complement: DataTypes.STRING,
+    conclused: DataTypes.BOOLEAN,
+    canceled: DataTypes.BOOLEAN,
+  });
+
+  return Colect;
+}

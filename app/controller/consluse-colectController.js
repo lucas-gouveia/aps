2d3b3356e364306885c430d0314364e06882ba88
Lucas Gouveia <lucasgouveia87@yahoo.com.br>
2021-11-18 21:46:13 -0300

    fix: conclused colect

--- app/controller/consluse-colectController.js
+++ app/controller/consluse-colectController.js
@@ -9,22 +9,20 @@ export const concluseColect = async (req, res) => {
         id: identificador.id,
       }
     })
-    if (userId !== colects.dataValues.user_id) {
-      return res.status(401).json({ message: 'Não autorizado!' })
-    }
     if (colects == null) {
       return res.status(400).json({ message: 'Coleta não encontrada!' })
     }
-    if (colects.dataValues.concluse == true) {
+    if (colects.dataValues.conclused == true) {
       return res.status(400).json({ message: 'Coleta já concluída!' })
     }
     if (colects.dataValues.canceled == true) {
       return res.status(400).json({ message: 'Coleta não pode ser concluída!' })
     }
-    colects.concluse = true
+    colects.conclused = true
     await colects.save()
     return res.json({ message: 'Coleta concluída!' })
   } catch (error) {
+    console.log(error)
     return res.status(500).json({ message: 'Erro ao concluir coleta!' })
   }
 }

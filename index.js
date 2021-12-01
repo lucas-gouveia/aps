1d778a782381c8042628f1123de632a5e742fe63
Lucas Gouveia <lucasgouveia87@yahoo.com.br>
2021-12-01 20:47:40 -0300

    cancel route

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
 app.post('/register', createUser)
 app.post('/login', login)
 app.post('/colect', verifyToken, createColect)
-app.post('/cancel', verifyToken, cancelColect)
+app.delete('/cancel/:id', verifyToken, cancelColect)
 app.post('/acept', verifyToken, aceptColect)
 app.post('/concluse', verifyToken, concluseColect)
 
